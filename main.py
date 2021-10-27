6b05610d098b1a0b92cc9cb82874a64bf1d4f99b
wilhelm wesser <wilhelmwesser228@gmail.com>
2021-10-27 10:38:06 +0300

    Power task(task 4) is done

--- main.py
+++ main.py
@@ -15,9 +15,15 @@ print(RelationProperties.reflection_antireflection(boolMatrix),
       RelationProperties.transitive(boolMatrix))
 print('\n Task 2')
 print(PropertyString.type_of_matrix(boolMatrix))
-print('\n Task 3')
+
+print('\n Task 3 \n Reflective Closure')
 Utils.print_matrix(Closures.reflective_closure(boolMatrix))
-print('\n')
+print('\n Symmetric Closure')
 Utils.print_matrix(Closures.symmetric_closure(boolMatrix))
-print('\n')
+print('\n Transitive closure')
 Utils.print_matrix(Closures.transitive_closure(boolMatrix))
+
+print('\n Task 4 \n Power 2')
+Utils.print_matrix(Utils.compose_matrix(boolMatrix, boolMatrix, 2))
+print('\n Power 3')
+Utils.print_matrix(Utils.compose_matrix(boolMatrix, boolMatrix, 3))
